96ca2750b63ce10c6f068468a46b624ba7b497e4
Antonio Mayrinck Moura (nthypes) <aasm.moura@gmail.com>
2021-01-10 22:47:15 -0300

    fix(tests): correct certificates path

--- tests/modules/Betfair/Account.test.ts
+++ tests/modules/Betfair/Account.test.ts
@@ -6,8 +6,8 @@ it ('Should pass with valid account credentials', async () => {
     password: process.env.BETFAIR_PASSWORD || '',
     apiKey: process.env.BETFAIR_API_KEY || '',
     certificateFilesPath: {
-      keyPath: '../../../../.secrets/betfair.key',
-      certPath: '../../../../.secrets/betfair.crt',
+      keyPath: '.secrets/betfair.key',
+      certPath: '.secrets/betfair.crt',
     },
   });
   expect (betfairAccount.sessionToken).toBeTruthy ();
